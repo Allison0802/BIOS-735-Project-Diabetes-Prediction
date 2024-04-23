f3df1a3e7b87da07f2d5e6747027e9128e7dd0d7
Allison0802 <yumeiy@ad.unc.edu>
2024-04-23 15:35:12 -0400

    updated the imputated datasets

--- predict.bios735/data-raw/diabetes_imputed.R
+++ predict.bios735/data-raw/diabetes_imputed.R
@@ -3,17 +3,16 @@ library(mice)
 library(caret)
 data <- diabetes[, -1]
 # split the data
+set.seed(1349)
 train_index <- createDataPartition(data$diabetes, p = 0.8, list = FALSE)
 train_data <- data[train_index, ]
 test_data <- data[-train_index, ]
 
 # impute training set and test set
-diabetes_train <- mice(data = train_data, seed = 1349, print = FALSE, m = 1)
-diabetes_train <- diabetes_train$data
+diabetes_train <- mice(data = train_data, seed = 1349, print = FALSE, m = 1) |> complete()
 use_data(diabetes_train, overwrite = TRUE)
 
-diabetes_test <- mice(data = test_data, seed = 1349, print = FALSE, m = 1)
-diabetes_test <- diabetes_test$data
+diabetes_test <- mice(data = test_data, seed = 1349, print = FALSE, m = 1) |> complete()
 use_data(diabetes_test, overwrite = TRUE)
 
 diabetes_complete <- na.omit(data)
